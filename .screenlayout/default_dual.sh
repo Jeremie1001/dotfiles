f71c1898055ace89a1982e8d1bf4aa030f72ea2a
Jeremie <jeremie.willis.cote@cern.ch>
2021-01-26 08:13:20 -0500

    Update used config folders

---
+++ .screenlayout/default_dual.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output DVI-D-1 --off --output HDMI-1 --mode 1920x1080 --pos 0x0 --rotate normal --output DP-1 --off --output DVI-D-1-2 --mode 1920x1080 --pos 1920x0 --rotate normal --output DP-1-2 --off --output DP-1-3 --off --output HDMI-1-2 --off --output DP-1-4 --off
