77e894697a2366185bfabc35c76517e98510cdad
Jeremie Cote <jeremie.w.cote@proton.me>
2024-04-26 02:35:24 -0400

    Renamed dot directories to 'dot_' in dotfiles directory for purpose of dotfile manager 'chezmoi'

--- .config/dmenu/confirm.sh
+++
@@ -1,1 +0,0 @@
-echo $(echo -e $@ | sed 's/^[^ ]* //' | sed "s/ /\n/g" | dmenu -i -p $1)
--- .config/dmenu/projectFolders.sh
+++
@@ -1,2 +0,0 @@
-chosen="$HOME/$(echo $(find ~/Documents/Projects -type d -print && find ~/Documents/School -type d -print && find ~/.config -type d -print) | tr " " "\n" | sed 's/\/home\/jeremie1001/~/' | dmenu -i -l 5 -p "Which folder" | sed 's/~\///')" || exit 1
-codium $chosen
